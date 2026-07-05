ca1193ff75db3923d4b7ad12dd01433d3bb99554
Qianqian Shao <110926331+qqs377@users.noreply.github.com>
2026-07-05 19:06:16 -0400

    Update global.js

--- global.js
+++ global.js
@@ -287,10 +287,10 @@ function logout() {
 // Background functions
 function setRandomBackground() {
     const backgrounds = [
+        "url('images/testing1.jpg')",
         "url('images/background1.png')",
         "url('images/background2.png')",
         "url('images/background3.png')",
-     //   "url('images/background4.png')",
         "url('images/background5.png')",
         "url('images/background6.png')",
         "url('images/wlop0.png')",
